1d208b2822ea4e0994d45f25a6160a9e11d71c33
P Czech <webcoderc@gmail.com>
2020-09-07 12:59:57 +0200

    Initial commit

---
+++ src/test/java/com/pczech/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pczech.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
