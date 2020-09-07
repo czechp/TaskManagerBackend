5f46769971ae5703c284379085a664564bb529ac
P Czech <webcoderc@gmail.com>
2020-09-07 15:35:32 +0200

    Basic security configuration

--- src/main/java/com/pczech/taskmanager/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/pczech/taskmanager/configuration/WebSecurityConfiguration.java
@@ -2,12 +2,24 @@ package com.pczech.taskmanager.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration()
-public class WebSecurityConfiguration {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/*").permitAll()
+                .antMatchers("/api/users/*").permitAll()
+                .anyRequest().authenticated();
+    }
 
     @Bean()
     public PasswordEncoder getPasswordEncoder() {
--- src/main/java/com/pczech/taskmanager/controller/AppUserController.java
+++ src/main/java/com/pczech/taskmanager/controller/AppUserController.java
@@ -1,11 +1,28 @@
 package com.pczech.taskmanager.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pczech.taskmanager.domain.AppUser;
+import com.pczech.taskmanager.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController()
 @RequestMapping("/api/users")
 @CrossOrigin("*")
 public class AppUserController {
+    private final AppUserService appUserService;
+
+    @Autowired()
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AppUser register(@RequestBody() @Valid() AppUser appUser, Errors errors) {
+        return appUserService.register(appUser, errors);
+    }
 }
