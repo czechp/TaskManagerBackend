e427deb0c0f91749e4a59ee8894a0007359f2d1b
P Czech <webcoderc@gmail.com>
2020-09-10 17:21:20 +0200

    Optimize imports and reformat code

--- src/main/java/com/pczech/taskmanager/interceptor/HibernateInterceptorComponent.java
+++ src/main/java/com/pczech/taskmanager/interceptor/HibernateInterceptorComponent.java
@@ -1,13 +1,8 @@
 package com.pczech.taskmanager.interceptor;
 
-import com.pczech.taskmanager.domain.AppUser;
 import org.hibernate.EmptyInterceptor;
-import org.hibernate.type.Type;
 import org.springframework.stereotype.Component;
-
-import java.io.Serializable;
 
 @Component()
 public class HibernateInterceptorComponent  extends EmptyInterceptor {
-
 }
