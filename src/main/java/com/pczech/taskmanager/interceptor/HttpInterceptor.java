fe2b47266bcd118e0206e4748ccb39f833a6a879
PCzech <you@example.com>
2020-09-22 22:13:04 +0200

    Resolve problem with CORS response status 0

---
+++ src/main/java/com/pczech/taskmanager/interceptor/HttpInterceptor.java
@@ -0,0 +1,21 @@
+package com.pczech.taskmanager.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component()
+public class HttpInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
+        return true;
+    }
+}
