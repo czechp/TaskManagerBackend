fc2d7861e1f78b086950e64e61a7a5c28f03f511
PCzech <you@example.com>
2020-09-24 21:51:35 +0200

    WebSocket configuration

--- src/main/java/com/pczech/taskmanager/aspect/advice/AspectAdvice.java
+++ src/main/java/com/pczech/taskmanager/aspect/advice/AspectAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect()
 @Component()
 public class AspectAdvice {
-    @AfterReturning(pointcut = "@annotation(com.pczech.taskmanager.aspect.annotation.TaskCreated)",
+    @AfterReturning(pointcut = "@annotation(com.pczech.taskmanager.aspect.annotation.ObjectCreatedAspect)",
             returning = "result")
     public void taskCreatedAdvice(Object result) {
 
---
+++ src/main/java/com/pczech/taskmanager/configuration/WebSocketConfiguration.java
@@ -0,0 +1,20 @@
+package com.pczech.taskmanager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration()
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/messaging");
+        registry.setApplicationDestinationPrefixes("app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/messaging-subscribe").withSockJS();
+    }
+}
