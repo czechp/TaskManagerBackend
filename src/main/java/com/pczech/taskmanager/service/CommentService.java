ad660efa946ef268f14f80ed207c66bea7572d39
P Czech <webcoderc@gmail.com>
2020-11-17 17:09:45 +0100

    Delete comments

--- src/main/java/com/pczech/taskmanager/service/CommentService.java
+++ src/main/java/com/pczech/taskmanager/service/CommentService.java
@@ -1,12 +1,5 @@
 package com.pczech.taskmanager.service;
 
-import com.pczech.taskmanager.domain.Comment;
-
-import java.util.List;
-
 public interface CommentService {
-    public Comment save(Comment comment);
-    public List<Comment> findAll();
-    public Comment findById(long id);
     public void deleteById(long id);
 }
--- src/main/java/com/pczech/taskmanager/service/CommentServiceImpl.java
+++ src/main/java/com/pczech/taskmanager/service/CommentServiceImpl.java
@@ -1,8 +1,10 @@
 package com.pczech.taskmanager.service;
 
 import com.pczech.taskmanager.domain.Comment;
+import com.pczech.taskmanager.exception.NotFoundException;
 import com.pczech.taskmanager.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,22 +20,11 @@ public class CommentServiceImpl implements CommentService{
     }
 
     @Override
-    public Comment save(Comment comment) {
-        return null;
-    }
-
-    @Override
-    public List<Comment> findAll() {
-        return null;
-    }
-
-    @Override
-    public Comment findById(long id) {
-        return null;
-    }
-
-    @Override
+    @CacheEvict(cacheNames = {"tasks"}, allEntries = true)
     public void deleteById(long id) {
-
+        if(commentRepository.existsById(id))
+            commentRepository.deleteById(id);
+        else
+            throw new NotFoundException("comment id --- " + id);
     }
 }
